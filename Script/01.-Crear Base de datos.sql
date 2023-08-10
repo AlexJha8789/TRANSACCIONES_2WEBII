bd8e6859405163fa96ec863ba94b3a743d97b5a6
unknown <alexjha.878@gmail.com>
2023-08-10 08:41:50 -0500

    first commit

---
+++ Script/01.-Crear Base de datos.sql
@@ -0,0 +1,7 @@
+-- borra la bd si existe
+DROP DATABASE IF EXISTS bd_semana07;
+-- creamos la bd
+CREATE DATABASE bd_semana07;
+-- activamos la bd
+USE bd_semana07;
+
---
+++ Script/02.-Script Insert.sql
@@ -0,0 +1,84 @@
+
+#INSERT PRODUCTOS
+INSERT INTO bd_semana07.tb_producto
+(id, descripcion, precio, stock)
+VALUES (null, 'Arroz', 2.99, 100);
+
+INSERT INTO bd_semana07.tb_producto
+(id, descripcion, precio, stock)
+VALUES (null, 'Frijoles', 1.99, 80);
+
+INSERT INTO bd_semana07.tb_producto
+(id, descripcion, precio, stock)
+VALUES (null, 'Azúcar', 2.49, 50);
+
+INSERT INTO bd_semana07.tb_producto
+(id, descripcion, precio, stock)
+VALUES (null, 'Sal', 0.99, 70);
+
+INSERT INTO bd_semana07.tb_producto
+(id, descripcion, precio, stock)
+VALUES (null, 'Aceite de oliva', 5.99, 30);
+
+INSERT INTO bd_semana07.tb_producto
+(id, descripcion, precio, stock)
+VALUES (null, 'Harina de trigo', 2.49, 60);
+
+INSERT INTO bd_semana07.tb_producto
+(id, descripcion, precio, stock)
+VALUES (null, 'Café', 3.99, 40);
+
+INSERT INTO bd_semana07.tb_producto
+(id, descripcion, precio, stock)
+VALUES (null, 'Leche', 1.99, 90);
+
+INSERT INTO bd_semana07.tb_producto
+(id, descripcion, precio, stock)
+VALUES (null, 'Pasta', 1.49, 75);
+
+INSERT INTO bd_semana07.tb_producto
+(id, descripcion, precio, stock)
+VALUES (null, 'Salsa de tomate', 1.99, 50);
+
+
+#INSERT CLIENTE
+INSERT INTO bd_semana07.tb_cliente
+(id, ape_paterno, ape_materno, nombres, dni, email, fecha_nacimiento)
+VALUES (null, 'López', 'García', 'Juan', '12345678', 'juanlopez@example.com', '1980-01-10');
+
+INSERT INTO bd_semana07.tb_cliente
+(id, ape_paterno, ape_materno, nombres, dni, email, fecha_nacimiento)
+VALUES (null, 'Gómez', 'Fernández', 'María', '87654321', 'mariagomez@example.com', '1981-02-11');
+
+INSERT INTO bd_semana07.tb_cliente
+(id, ape_paterno, ape_materno, nombres, dni, email, fecha_nacimiento)
+VALUES (null, 'Rodríguez', 'Pérez', 'Luis', '56789012', 'luisrodriguez@example.com', '1982-03-12');
+
+INSERT INTO bd_semana07.tb_cliente
+(id, ape_paterno, ape_materno, nombres, dni, email, fecha_nacimiento)
+VALUES (null, 'Martínez', 'López', 'Ana', '90123456', 'anamartinez@example.com', '1983-04-13');
+
+INSERT INTO bd_semana07.tb_cliente
+(id, ape_paterno, ape_materno, nombres, dni, email, fecha_nacimiento)
+VALUES (null, 'Hernández', 'Gómez', 'Pedro', '65432109', 'pedrohernandez@example.com', '1984-05-14');
+
+INSERT INTO bd_semana07.tb_cliente
+(id, ape_paterno, ape_materno, nombres, dni, email, fecha_nacimiento)
+VALUES (null, 'Pérez', 'Rodríguez', 'Carolina', '89012345', 'carolinaperez@example.com', '1985-06-15');
+
+INSERT INTO bd_semana07.tb_cliente
+(id, ape_paterno, ape_materno, nombres, dni, email, fecha_nacimiento)
+VALUES (null, 'López', 'González', 'Manuel', '54321098', 'manuellopez@example.com', '1986-07-16');
+
+INSERT INTO bd_semana07.tb_cliente
+(id, ape_paterno, ape_materno, nombres, dni, email, fecha_nacimiento)
+VALUES (null, 'Fernández', 'Hernández', 'Sara', '01234567', 'sarafernandez@example.com', '1987-08-17');
+
+INSERT INTO bd_semana07.tb_cliente
+(id, ape_paterno, ape_materno, nombres, dni, email, fecha_nacimiento)
+VALUES (null, 'González', 'Martínez', 'Ricardo', '78901234', 'ricardogonzalez@example.com', '1988-09-18');
+
+
+
+
+
